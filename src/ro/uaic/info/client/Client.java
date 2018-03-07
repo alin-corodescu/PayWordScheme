b84efc592ceae6068a449c5a6bc8790a3c52fc67
alin-corodescu <alin.corodescu@gmail.com>
2018-03-07 10:58:31 +0200

    intermediary push

--- src/ro/uaic/info/client/Client.java
+++ src/ro/uaic/info/client/Client.java
@@ -4,4 +4,22 @@ package ro.uaic.info.client;
  * Created by alin on 3/7/18.
  */
 public class Client {
+    Object brokerSignature;
+    Object brokerIdentity;
+    Object clientIdentity;
+    Object brokerPublicKey;
+    Object userPublicKey;
+    Object expirationDate;
+    Object info;
+    /**
+     * Connects to the localhost:port broker and registers itself with it
+     * @param port
+     */
+    void registerWithBroker(int port) {
+
+    }
+
+    void verifySignature
+
+
 }
---
+++ src/ro/uaic/info/crypto/Signature.java
@@ -0,0 +1,11 @@
+package ro.uaic.info.crypto;
+
+/**
+ * Created by alin on 3/7/18.
+ */
+public class Signature {
+//
+    boolean checkSignature(String message, String , Object publicKey) {
+        return false;
+    }
+}
